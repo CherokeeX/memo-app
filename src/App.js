91103d8e8215eb39a11051146bbc7e440beb4194
CherokeeX <serefb80@gmail.com>
2023-04-13 10:43:48 +0300

    cardLists

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+
 
 
 const cardList = [
   {'path ': '/img/1.jpeg'},
   {'path ': '/img/2.jpeg'},
   {'path ': '/img/3.jpeg'},
-  {'path ': '/i
+  {'path ': '/img/4.jpeg'},
+  {'path ': '/img/5.jpeg'},
+  {'path ': '/img/6.jpeg'},
+]
+
+
+
+function App() {
+
+  const [cards,setCards ] = useState([])
+
+  setCards(cardList)
+  return (
+    <div className="App">
+      <h1>Memory App</h1>
+    </div>
+  );
+}
+
+export default App;
